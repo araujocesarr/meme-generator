e7536c69aa71fb84d46dfc96944c97ac88650814
CesarAraujo <cesararaujo.perez@gmail.com>
2023-07-17 13:35:10 -0400

    Updates

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -10,7 +10,10 @@ export default function Meme() {
   });
 
   const [allMemeImages, setAllMemeImages] = useState(memesData);
-  function getMemeImage() {
+
+  function getMemeImage(event) {
+    event.preventDefault();
+    console.log(meme);
     const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
@@ -19,6 +22,14 @@ export default function Meme() {
       randomImage: url,
     }));
     console.log(meme);
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
   }
 
   return (
@@ -31,12 +42,20 @@ export default function Meme() {
           type="text"
           placeholder="Top text"
           className="indent-3 h-9 rounded-md border border-solid border-gray-300"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         />
+
         <input
           type="text"
           placeholder="Bottom text"
           className="indent-3 h-9 rounded-md border border-solid border-gray-300"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
+
         <button
           onClick={getMemeImage}
           className="col-span-2 
@@ -46,7 +65,36 @@ export default function Meme() {
           Get a new meme image
         </button>
       </form>
-      <img src={meme.randomImage} alt="" />
+
+      <div className="relative">
+        <img
+          src={meme.randomImage}
+          alt=""
+          className="max-w-full mt-5 rounded-lg"
+        />
+
+        <h2
+          className="absolute w-4/5 
+        text-center left-1/2 translate-x-[-50%]
+        my-4 mx-0 py-0 px-1 top-0
+        font-impact text-[2em]
+        uppercase text-white
+        tracking-[1px]"
+        >
+          {meme.topText}
+        </h2>
+
+        <h2
+          className="absolute w-4/5 
+        text-center left-1/2 translate-x-[-50%]
+        my-4 mx-0 py-0 px-1 bottom-0
+        font-impact text-[2em]
+        uppercase text-white
+        tracking-[1px]"
+        >
+          {meme.bottomText}
+        </h2>
+      </div>
     </main>
   );
 }
